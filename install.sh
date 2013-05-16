e8194f62f3cd8889f0efe1bb83916be9d1c63ad6
Cleyton Fabio <pixies@pelivre.org>
2013-05-16 18:50:26 -0300

    Script de instalacao do bee-v0.1

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#Criando diretorio dos vhosts
+echo 'Add vhost dir'
+mkdir vhosts
+
+#Backup do apache2.conf
+echo 'Add backup apache2.conf -> apache2.conf.backup.bee'
+cp -r /etc/apache2/apache2.conf /etc/apache2/apache2.conf.backup.bee
+
+#Adicionando -> Include /opt/bee/vhosts
+echo 'Add Include vhost directory into apache2.conf'
+echo "
+#Adicionando diretorio vhosts ao bee
+Include /opt/bee/vhosts" >> /etc/apache2/apache2.conf
+
+#Restartando o apache
+echo 'Restart apache2'
+apache2ctl restart
