d39eb7c7d9f4579e8743a6aac6e80dd735a07e3e
Cleyton Fabio <pixies@pelivre.org>
2013-05-24 21:45:13 -0300

    Corrigindo script install.sh (retirando atributo -e do echo)

--- install.sh
+++ install.sh
@@ -23,34 +23,34 @@ MKDIR="$(which mkdir)"
 CP="$(which cp)"
 APACHECTL="$(which apachectl)"
 
-#### INSTALL 
+#### INSTALL
 
 if [ -d "$CONF_APACHE" ]; then
 
 		if [ ! -d "$CONF_BEE" ]; then
-			echo -e "\nInstall Bee."
-			$MKDIR -p $CONF_BEE/vhosts/ 
+			echo "\nInstall Bee."
+			$MKDIR -p $CONF_BEE/vhosts/
 
 			echo "Backup apache2.conf -> $CONF_APACHE/apache2.conf_backup.bee"
 			$CP $CONF_APACHE/apache2.conf $CONF_APACHE/apache2.conf_backup.bee
 
 			## Adicionando -> Include /etc/bee/vhosts
 			echo "Adding directory "Include vhost" in apache2.conf."
-			echo -e "\n## VHosts directory\nInclude $CONF_BEE/vhosts" >> $CONF_APACHE/apache2.conf
+			echo "\n## VHosts directory\nInclude $CONF_BEE/vhosts" >> $CONF_APACHE/apache2.conf
 
 			## Restartando o apache
 			echo "Restart apache2."
-			
+
 			$APACHECTL restart
-			
-			echo -e "OK\n"
+
+			echo "OK\n"
 
 	  	else
-			echo -e "\nDirectory "$CONF_BEE" already exists.\nPlease change the variable "CONF_BEE".\n"
+			echo "\nDirectory "$CONF_BEE" already exists.\nPlease change the variable "CONF_BEE".\n"
 		fi
 
   else
 
-     echo -e "\nDirectory "$CONF_APACHE" does not exist.\nPlease install "apache2" ( if System Operation Debian GNU/Linux, then, apt-get install apache2 ).\n"
+     echo -e "\nDirectory "$CONF_APACHE" does not exist.\nPlease install "apache2" ( apt-get install apache2 ).\n"
 
 fi
